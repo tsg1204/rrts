7d7db568536beeca1ce41057052bf5390ad101ef
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-19 17:29:09 -0500

    refactor redux store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+  componentDidMount(){
+    this.props.fetchTodos();
+  }
+
   render() {
+    console.log(this.props.todos);
     return <div>Hi from App</div>
   }
 }
+
+const mapStateToProps = (state: StoreState): { todos: Todo[]} => {
+  return { todos: state.todos}
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
