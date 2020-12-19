3134216acfe71c1f2021025cf3f0eafbee46f75f
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-19 17:38:00 -0500

    add render list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Todo, fetchTodos } from '../actions';
 import { StoreState } from '../reducers';
+import { todosReducer } from '../reducers/todos';
 
 interface AppProps {
   todos: Todo[];
@@ -9,13 +10,24 @@ interface AppProps {
 }
 
 class _App extends React.Component<AppProps> {
-  componentDidMount(){
+
+  onButtonClick = (): void => {
     this.props.fetchTodos();
   }
 
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>
+    })
+  }
+
   render() {
-    console.log(this.props.todos);
-    return <div>Hi from App</div>
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    )
   }
 }
 
