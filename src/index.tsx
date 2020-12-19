411c084ba6f47a7b24a6274c977fd9baa42b0201
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-19 16:26:56 -0500

    actions fetch method  reducers added

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-  color: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-class App extends React.Component<AppProps> {
-  render() {
-    return <div>{this.props.color}</div>
-  }
-}
 
-ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, 
+    document.querySelector('#root')
+  );
+
+
+//ho to define functional component 
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>
+// }
